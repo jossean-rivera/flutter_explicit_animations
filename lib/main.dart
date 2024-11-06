7d482f814b2c2977c528d5c526fcd402dfaa90ae
Jossean Rivera <jossean.rivera@outlook.com>
2024-11-05 16:16:14 -0800

    Step 7 code

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,36 @@ class LogoApp extends StatefulWidget {
   State<LogoApp> createState() => _LogoAppState();
 }
 
-//  Use the AnimatedWidget helper class that will
-//  rebuild everytime the animation calls the listener
-//  No need to set the state of the widget directly.
 class AnimatedLogo extends AnimatedWidget {
   const AnimatedLogo({super.key, required Animation<double> animation})
       : super(listenable: animation);
 
+  static const double _maxSize = 300;
+  static const double _minSize = 50;
+
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: _minSize, end: _maxSize);
+
   @override
   Widget build(BuildContext context) {
     final animation = listenable as Animation<double>;
-    return Center(
-      child: SizedBox(
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
+    return Opacity(
+      //  Use the tween to change the animation value, to an opacity value
+      opacity: _opacityTween.evaluate(animation),
+      child: Center(
+        child: SizedBox(
+          //  Use the tween to change the animation value, to a size value
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
       ),
     );
   }
 }
 
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  final double _maxSize = 300;
-  final double _minSize = 50;
   final double _animationStep = 0.1;
 
   late AnimationController controller;
@@ -64,22 +71,20 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     //  Set a loop animation that increases and decreases the logo from
     //  _minSize to _maxSize
     //  Set a curve of the animation
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeOut);
-    animation =
-        Tween<double>(begin: _minSize, end: _maxSize).animate(controller)
-          ..addStatusListener((status) {
-            // Use addStatusListener to loop the animation.
-            switch (status) {
-              case AnimationStatus.completed:
-                controller.reverse();
-                break;
-              case AnimationStatus.dismissed:
-                controller.forward();
-                break;
-              // Do nothing for other statuses (forwarding or reversing).
-              default:
-            }
-          });
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeOut)
+      ..addStatusListener((status) {
+        // Use addStatusListener to loop the animation.
+        switch (status) {
+          case AnimationStatus.completed:
+            controller.reverse();
+            break;
+          case AnimationStatus.dismissed:
+            controller.forward();
+            break;
+          // Do nothing for other statuses (forwarding or reversing).
+          default:
+        }
+      });
   }
 
   @override
