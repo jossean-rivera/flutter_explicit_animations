0462c693980e4f1ec998ea6660f2d2fef2323c3a
Jossean Rivera <jossean.rivera@outlook.com>
2024-11-05 16:07:08 -0800

    Step 5 code

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ class AnimatedLogo extends AnimatedWidget {
   }
 }
 
-
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   final double _maxSize = 300;
   final double _minSize = 50;
@@ -61,8 +60,27 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    //  Set a loop animation that increases and decreases the logo from
+    //  _minSize to _maxSize
     animation =
-        Tween<double>(begin: _minSize, end: _maxSize).animate(controller);
+        Tween<double>(begin: _minSize, end: _maxSize).animate(controller)
+          ..addStatusListener((status) {
+            // The addStatusListener checks the current status of the animation.
+            switch (status) {
+              // If the animation has completed, reverse it back to the start.
+              case AnimationStatus.completed:
+                controller.reverse();
+                break;
+
+              // If the animation has been dismissed (reached the start), start it again forward.
+              case AnimationStatus.dismissed:
+                controller.forward();
+                break;
+
+              // Do nothing for other statuses (forwarding or reversing).
+              default:
+            }
+          });
   }
 
   @override
